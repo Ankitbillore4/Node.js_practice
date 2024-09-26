f401b9c3190af45a0c06d8886ee9804b5ee48375
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-09-26 13:59:24 +0530

    Append File

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // for creating a file 
-
 const fs = require('fs');
 
 fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
     if(err) console.log(err);
     else console.log("file written successfully");
+})
+
+
+//append File (bani banai file me aagey kuch add karna )
+fs.appendFile("hello.txt","  Kha Chalo",(err)=>{
+if(err){
+    console.log(err);
+
+}else{
+    console.log("Appended Sucesfully");
+}
 })
