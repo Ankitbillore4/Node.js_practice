1a73b5c0835ee4ef893a3ef845e9ba964edb3b43
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-09-26 14:08:41 +0530

    js  changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// for creating a file 
+// for creating a file                                   //code script.js(vs code me lejaygi)
 const fs = require('fs');
 
 fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
@@ -7,7 +7,7 @@ fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
 })
 
 
-//append File (bani banai file me aagey kuch add karna )
+//for append File (bani banai file me aagey kuch add karna )
 fs.appendFile("hello.txt","  Kha Chalo",(err)=>{
 if(err){
     console.log(err);
@@ -15,4 +15,14 @@ if(err){
 }else{
     console.log("Appended Sucesfully");
 }
+})
+
+
+fs.rename("hello.txt","helooow1.txt",(err)=>{
+    if(err){
+        console.log(err);
+
+    }else{
+        console.log("Rename Sucesfully");
+    }
 })
