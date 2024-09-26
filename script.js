d521a6f0115dfe92a0f030718d450903c71445af
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-09-26 14:30:37 +0530

    js changes

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-// for creating a file                                   //code script.js(vs code me lejaygi)
-const { log } = require('console');
-const fs = require('fs');
 
-fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
-    if(err) console.log(err);
-    else console.log("file written successfully");
-})
+const fs = require('fs');                                //code script.js(vs code me lejaygi)
 
 
-//for append File (bani banai file me aagey kuch add karna )
-fs.appendFile("hello.txt","  Kha Chalo",(err)=>{
-if(err){
-    console.log(err);
+// // for creating a file                                   
 
-}else{
-    console.log("Appended Sucesfully");
-}
-})
+// fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
+//     if(err) console.log(err);
+//     else console.log("file written successfully");
+// })
 
-// for rename a file 
-fs.rename("hello.txt","helooow1.txt",(err)=>{
-    if(err){
-        console.log(err);
 
-    }else{
-        console.log("Rename Sucesfully");
-    }
-})
+// //for append File (bani banai file me aagey kuch add karna )
+// fs.appendFile("hello.txt","  Kha Chalo",(err)=>{
+// if(err){
+//     console.log(err);
+
+// }else{
+//     console.log("Appended Sucesfully");
+// }
+// })
+
+// // for rename a file 
+// fs.rename("hello.txt","helooow1.txt",(err)=>{
+//     if(err){
+//         console.log(err);
+
+//     }else{
+//         console.log("Rename Sucesfully");
+//     }
+// })
+
+//for read a file 
+// fs.readFile("hello.txt","utf-8",(err,data)=>{
+    // if(err) console.log(err);
+    // else console.log(data);
+// })
+// 
+
 
 // for delete a file 
-
-fs.unlink("heloow1.txt",(err)=>{
-if(err){
-    console.log(err);
-
-}else{
-    console.log("File Deleted");
-}
+fs.unlink("hello.txt",(err)=>{
+    if(err) console.log(err);
+    else console.log("File Deleted");
 })
+
