9fcea39df83b27f9be88aab16ea10a914710255d
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-09-26 13:34:17 +0530

    Changes

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const fs = require('fs');
-
-fs.writeFile("hello.txt","hii helo",(err)=>{
-    if(err) console.log(err);
-    else console.log("file written successfully");
-})
 // for creating a file 
 
 const fs = require('fs');
-fs.readFile("helo.txt","utf-8",(err,data)=>{
- if(err) console.log(err);
- else console.log(data);
+
+fs.writeFile("hello.txt","hii helo Chaloo",(err)=>{
+    if(err) console.log(err);
+    else console.log("file written successfully");
 })
-// for reading a file 
-// 
-// const fs = require('fs');
-// fs.appendFile("hello.txt","hii helo",(err)=>{
-    // if(err) console.log(err);
-    // else console.log("file written successfully");
-// })
-// 
-// 
