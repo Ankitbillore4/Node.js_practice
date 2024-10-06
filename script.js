c6e5d55d6f62cd2ba0eaca64747c865c7e841911
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-10-06 12:53:08 +0530

    changes

--- script.js
+++ script.js
@@ -174,11 +174,11 @@
 // const express = require("express")
 // const app = express();
 
-app.set("view engine","ejs");
-
-app.get("/", function(req,res){
-    res.render("index");
-})
+// app.set("view engine","ejs");
+// 
+// app.get("/", function(req,res){
+    // res.render("index");
+// })
 
 app.get("/About", function(req,res){
     res.render("About");
