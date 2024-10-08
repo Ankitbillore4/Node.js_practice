fdfae46bf963f9fe280603fe9dbd7a5e774dd217
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-10-08 21:30:07 +0530

    app.js  changes

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.get("/contact/:id",function(req,res){
-    res.render(`${req.params.id} is your id :`)
+    res.render("contact",`${req.params.id} is your id `)
 })
 
 app.listen(3000);
